d032de17b003c139b34c982f367405dc28a6cf30
Sumair Uddin Siddiqui <sumair.siddiqui.47@gmail.com>
2025-05-18 16:31:52 +0500

    Update the file

---
+++ connection.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+pymysql://root:127.0.0.1:3306/Movie_RES_SYS"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
