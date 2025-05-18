4ad564d8b3f22983b80aedbdfe7a74d5fa50f04e
Sumair Uddin Siddiqui <sumair.siddiqui.47@gmail.com>
2025-05-18 17:01:40 +0500

    Update file

--- Model/Database_Model.py
+++ Model/Database_Model.py
@@ -1,4 +1,22 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.mssql import INTEGER
 
 BASE = declarative_base()
 
+class User(BASE):
+    __tablename__ = "User_details"
+    ID = Column(
+        INTEGER(unsigned = True),
+        autoincrement= True,
+        nullable= False
+        )
+    Name = Column(
+        String(50),
+        nullable= False
+        )
+    Email = Column(
+        String,
+        nullable=False
+        )
+    
