529dec04bd143ee897552d9b44abadc7ec39ec24
Sumair Uddin Siddiqui <sumair.siddiqui.47@gmail.com>
2025-07-04 18:20:59 +0500

    Create Life span

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 from .connection import engine
 from .Model import Database_Model
 from .routes import routes
+from contextlib import asynccontextmanager
 
-Database_Model.BASE.metadata.create_all(engine)
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print("Server is starting.....")
+    Database_Model.BASE.metadata.create_all(engine)
+    yield
+    print("Server has been shutdown")
+
+
 
 app = FastAPI (
     title = "CineVerse",
-    description = "Movie Reservation System"
+    description = "Movie Reservation System",
+    lifespan= life_span
 )
 
 app.include_router(routes.router)
