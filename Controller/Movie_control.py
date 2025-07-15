8f508a3fdadbb90e8e0f83139aac8d05dbd36fe7
Sumair Uddin Siddiqui <sumair.siddiqui.47@gmail.com>
2025-07-15 13:44:37 +0500

    Add Movie end-point

--- Controller/Movie_control.py
+++ Controller/Movie_control.py
@@ -7,3 +7,16 @@ def movies(db):
 
 def get_a_movie(movie_name,db):
     return db.query(Database_Model.Movie).filter(Database_Model.Movie.Title == movie_name).first()
+
+def get_movie(db):
+    return db.query(Database_Model.Movie)
+
+def get_genre(db, genre:str):
+    query = db.query(Database_Model.Movie)
+    query = query.filter(Database_Model.Movie.Genre.ilike(f"%{genre}%"))
+    return query
+
+def get_language(db, language:str):
+    query = get_movie(db)
+    query = query.filter(Database_Model.Movie.Language.ilike(f"%{language}%"))
+    return query
--- routes/movie_routes.py
+++ routes/movie_routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, status
-from typing import List
+from typing import List, Optional
 from Model import Pydantic_Model
 from utils import dependencies
 from Controller import Movie_control
+from Model import Database_Model
 
 router = APIRouter(prefix="/movie", tags=["Movie"])
 
@@ -22,3 +23,24 @@ async def get_all_movies(db: dependencies.db_dependency):
 async def get_movie(movie_name:str, db:dependencies.db_dependency):
     get_film = Movie_control.get_a_movie(movie_name,db)
     return get_film
+
+@router.get("/movies_filter",
+            response_model=List[Pydantic_Model.Movie_response],
+            status_code=status.HTTP_302_FOUND)
+
+async def movie_filter(
+    *, # serach all the arguments pass in this function 
+    genre: Optional[str] = None,
+    language: Optional[str] = None,
+    db: dependencies.db_dependency
+):
+    # query =  Movie_control.get_movie
+    query = db.query(Database_Model.Movie)
+
+    if genre:
+        query = Movie_control.get_genre(db, genre)
+
+    if language:
+        query = Movie_control.get_language(db, language)
+
+    return query.all()
