4e5463bf71cdd6741fd44b253a1967b4000c2885
Sumair Uddin Siddiqui <sumair.siddiqui.47@gmail.com>
2025-07-16 23:07:15 +0500

    Update the Database

--- Model/Database_Model.py
+++ Model/Database_Model.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, PrimaryKeyConstraint, func
-from sqlalchemy.dialects.mysql import INTEGER, BIGINT, DATE, BOOLEAN
+from sqlalchemy import Column, String, PrimaryKeyConstraint, func, ForeignKey
+from sqlalchemy.dialects.mysql import INTEGER, BIGINT, DATE, BOOLEAN, DATETIME, FLOAT
+from datetime import datetime
+from sqlalchemy.orm import relationship
 
 BASE = declarative_base()
 
@@ -41,6 +43,8 @@ class User(BASE):
         PrimaryKeyConstraint(ID,Email),
     )
     
+    reservation = relationship("Reservation", back_populates="User")
+
 #  Movie Table
 
 class Movie(BASE):
@@ -83,4 +87,138 @@ class Movie(BASE):
     __table_args__ = (
         PrimaryKeyConstraint(ID,Title),
     )
+    
+    showtime = relationship("Showtime", back_populates="Movie")
+
+# Screen table 
+
+class Screen(BASE):
+    __tablename__ = "Screens"
+
+    ID = Column(
+        INTEGER,
+        primary_key=True,
+        unique=True,
+        nullable=False
+    )
+    Name = Column(
+        String,
+        unique=True,
+        nullable= True
+    )
+
+    seats = relationship("Seat", back_populates="Screens")
+    showtime = relationship("Showtime", back_populates="Screen")
+
+
+# Showtime table
+
+class Showtime(BASE):
+    __tablename__ = "Showtime"
+    
+    ID = Column(
+        INTEGER,
+        autoincrement=True,
+        nullable = False,
+        unique= True
+    )
+    Movie_ID = Column(
+        INTEGER,
+        ForeignKey("Movie_Table.ID"),
+        nullable= False
+    )
+    Start_time = Column(
+        DATETIME,
+        nullable= False
+    )
+    Screen = Column(
+        String
+    )
+    
+    movie = relationship("Movie", back_populates="Showtime")
+    screen = relationship("Screen", back_populates="showtime")
+    reservation = relationship("Reservation", back_populates="Showtime")
+
+class Seats(BASE):
+    __tablename__ = "Seats"
+
+    ID = Column(
+        INTEGER,
+        primary_key= True,
+        nullable= False,
+        unique= True
+    )
+    Showtime_ID = Column(
+        INTEGER,
+        ForeignKey("Showtime.ID")
+    )
+    Seat_number = Column(
+        String,
+        nullable= False
+    )
+
+    screen = relationship("Screen", back_populates="Seats")
+    reservation_seats = relationship("Reservation", back_populates="Seats")
+
+
+class Reservation(BASE):
+    __tablename__ = "Reservation"
+
+    ID = Column(
+        INTEGER,
+        primary_key=True,
+        unique= True,
+        nullable= False
+    )
+    User_ID = Column(
+        INTEGER,
+        ForeignKey("User_Table.ID")
+    )
+    Showtime_ID = Column(
+        INTEGER,
+        ForeignKey("Showtime.ID")
+    )
+    Created_at = Column(
+        DATETIME,
+        default=datetime.utcnow
+    )
+    Total_price = Column(
+        FLOAT,
+        default=1000
+    )
+    Status = Column(
+        String,
+        default="confirmed"
+    )
+
+    user = relationship("User", back_populates="Reservation")
+    showtime = relationship("Showtime", back_populates="Reservation")
+    reservation_seats = relationship("Reservation_Seats", back_populates="Reservation")
+    
+class Reservation_Seat(BASE):
+    __tablename__ = "Reservated_Seats"
+
+    ID = Column(
+        INTEGER,
+        primary_key=True,
+        unique=True,
+        nullable= True
+    )
+    Reservation_ID = Column(
+        INTEGER,
+        ForeignKey("Reservation.ID")
+    )
+    Seat_ID = Column(
+        INTEGER,
+        ForeignKey("Seats.ID")
+    )
+    Showtime_ID = Column(
+        INTEGER,
+        ForeignKey("Reservation.ID"),
+        nullable=False
+    )
+    
+    reservation = relationship("Reservation", back_populates="Reservation_Seats")
+    seat = relationship("Seats", back_populates="Reservation_Seats")
+    showtime = relationship("Showtime")
     
--- routes/user_routes.py
+++ routes/user_routes.py
@@ -9,6 +9,7 @@ router = APIRouter(prefix="/user", tags=["User"])
 @router.get("/account_details/{email}",
             response_model=Pydantic_Model.User_response,
             status_code=status.HTTP_302_FOUND)
+
 async def User_details(email:str, db:dependencies.db_dependency): # type: ignore
     get_user = User_control.get_user_details(email, db)
     return get_user
